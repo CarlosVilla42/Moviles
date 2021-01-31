fcfb776fed0cbf4c568469cfa26d58cec53289ac
CarlosVilla42 <47682711+CarlosVilla42@users.noreply.github.com>
2021-01-31 13:37:38 -0600

    app contactos

---
+++ Contactos9-10/Contactos9-10/MiContacto.swift
@@ -0,0 +1,16 @@
+//
+//  MiContacto.swift
+//  Contactos9-10
+//
+//  Created by Mac16
+
+//
+
+import Foundation
+
+struct MiContacto {
+    var nombre: String?
+    var telefono: Int64?
+    var direccion : String?
+    
+}
